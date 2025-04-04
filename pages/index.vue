60ca2499219b07832c231d3cfd8bc2a006fb94b4
Nimira43 <n.rabone72@aol.com>
2025-04-04 03:23:03 +0100

    Dynamic routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-link to="/about">About</nuxt-link>
   <nuxt-link to="/notes" target="_blank">Notes</nuxt-link>
-<h1>Project Nuxt</h1>
+  <h1>Project Nuxt</h1>
 </template>
 
 <script>

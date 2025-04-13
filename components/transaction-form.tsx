abc80193d5600254a5042460953ab13e7c5400ea
Nimira43 <n.rabone72@aol.com>
2025-04-13 20:58:47 +0100

    Ensuring Zod and useForm are working together

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -23,8 +23,14 @@ const transactionFormSchema = z.object({
 })
 
 export default function TransctionForm() {
-  const form = useForm<z.infer<typeof transactionFormSchema>>()
+  const form = useForm<z.infer<typeof transactionFormSchema>>({
+    resolver: zodResolver(transactionFormSchema)
+  })
   return (
     <div>TransctionForm</div>
   )
+}
+
+function zodResolver(transactionFormSchema: z.ZodObject<{ transactionType: z.ZodEnum<["income", "expense"]>; categoryId: z.ZodNumber; transactionDate: z.ZodDate; amount: z.ZodNumber; description: z.ZodString }, "strip", z.ZodTypeAny, { transactionType: "income" | "expense"; categoryId: number; transactionDate: Date; amount: number; description: string }, { transactionType: "income" | "expense"; categoryId: number; transactionDate: Date; amount: number; description: string }>): import("react-hook-form").Resolver<{ transactionType: "income" | "expense"; categoryId: number; transactionDate: Date; amount: number; description: string }, any, { transactionType: "income" | "expense"; categoryId: number; transactionDate: Date; amount: number; description: string }> | undefined {
+  throw new Error('Function not implemented.')
 }

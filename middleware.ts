11747386d5bc5d3e8a18bba91fe3c88a8b5d322e
Nimira43 <n.rabone72@aol.com>
2025-04-13 20:00:28 +0100

    Created callback function with clerkMiddleware checking isProtectedRoute

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,11 @@ const isProtectedRoute = createRouteMatcher([
   '/dashboard(.*)'
 ])
 
-
-export default clerkMiddleware()
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [

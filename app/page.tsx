d78dc20cba96e1ff5c97239c6dd4bd3223834eab
Nimira43 <n.rabone72@aol.com>
2025-04-13 17:40:56 +0100

    Udong global variable to implement Poppins font

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='p-5 bg-dark'>
-      <h1 className='logo text-main text-3xl'>Libra</h1>
-    </div>
+    <>
+      <div className='p-5 bg-dark'>
+        <h1 className='logo text-main text-3xl'>Libra</h1>
+      </div>
+      <div className='p-5'>
+        <p>Testing</p>
+      </div>
+    </>
   )
 }
 

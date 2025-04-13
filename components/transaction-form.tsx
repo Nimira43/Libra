6e2491cc617df6c3ddbec08d077dfc539fffefec
Nimira43 <n.rabone72@aol.com>
2025-04-13 21:34:10 +0100

    ZodResolver

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -3,6 +3,7 @@
 import { addDays } from 'date-fns'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const transactionFormSchema = z.object({
   transactionType: z
@@ -29,8 +30,4 @@ export default function TransctionForm() {
   return (
     <div>TransctionForm</div>
   )
-}
-
-function zodResolver(transactionFormSchema: z.ZodObject<{ transactionType: z.ZodEnum<["income", "expense"]>; categoryId: z.ZodNumber; transactionDate: z.ZodDate; amount: z.ZodNumber; description: z.ZodString }, "strip", z.ZodTypeAny, { transactionType: "income" | "expense"; categoryId: number; transactionDate: Date; amount: number; description: string }, { transactionType: "income" | "expense"; categoryId: number; transactionDate: Date; amount: number; description: string }>): import("react-hook-form").Resolver<{ transactionType: "income" | "expense"; categoryId: number; transactionDate: Date; amount: number; description: string }, any, { transactionType: "income" | "expense"; categoryId: number; transactionDate: Date; amount: number; description: string }> | undefined {
-  throw new Error('Function not implemented.')
 }

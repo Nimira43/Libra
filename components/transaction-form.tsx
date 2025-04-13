ca7b1e2862027e54c7f431ffb7040a187db58c35
Nimira43 <n.rabone72@aol.com>
2025-04-13 20:46:04 +0100

    Completed transactionDate field which constrains the chosen date not to be in the future.

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -1,12 +1,12 @@
 'use client'
 
+import { addDays } from 'date-fns'
 import { z } from 'zod'
 
 const transactionFormSchema = z.object({
   transactionType: z.enum(['income', 'expense']),
   categoryId: z.coerce.number().positive('Please select a category'),
-  transactionDate: z.coerce.date().max()
-
+  transactionDate: z.coerce.date().max(addDays(new Date(), 1), 'Transaction date cannot be in the future')
 })
 
 export default function TransctionForm() {

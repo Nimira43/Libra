bb9bb77206eb66adf249892a0fd5578607c433c8
Nimira43 <n.rabone72@aol.com>
2025-04-13 22:17:57 +0100

    Added light bg colour.

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -61,7 +61,7 @@ export default function TransctionForm() {
                     <SelectTrigger>
                       <SelectValue />
                     </SelectTrigger>
-                    <SelectContent>
+                    <SelectContent className='bg-light'>
                       <SelectItem value='income'>
                         Income
                       </SelectItem>

922dc3be5b126934972e8470dab66467548e3fb3
Nimira43 <n.rabone72@aol.com>
2025-04-13 20:52:06 +0100

    Added amount and description to schema

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -4,9 +4,21 @@ import { addDays } from 'date-fns'
 import { z } from 'zod'
 
 const transactionFormSchema = z.object({
-  transactionType: z.enum(['income', 'expense']),
-  categoryId: z.coerce.number().positive('Please select a category'),
-  transactionDate: z.coerce.date().max(addDays(new Date(), 1), 'Transaction date cannot be in the future')
+  transactionType: z
+    .enum(['income', 'expense']),
+  categoryId: z.coerce
+    .number()
+    .positive('Please select a category.'),
+  transactionDate: z.coerce
+    .date()
+    .max(addDays(new Date(), 1), 'Transaction date cannot be in the future.'),
+  amount: z.coerce
+    .number()
+    .positive('Amount must be greater than 0.'),
+  description: z
+    .string()
+    .min(3, 'Description must contain at least 3 characters')
+    .max(300, 'Description must contain no more than 300 characters')
 })
 
 export default function TransctionForm() {

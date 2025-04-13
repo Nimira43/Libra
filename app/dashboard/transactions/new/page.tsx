6b06cc96081c9ab66aac0a658e9f8cca1e14d452
Nimira43 <n.rabone72@aol.com>
2025-04-13 19:35:44 +0100

    Brought in BreadCrumb component into NewTransactionPage.

--- app/dashboard/transactions/new/page.tsx
+++ app/dashboard/transactions/new/page.tsx
@@ -1,5 +1,9 @@
+import { Breadcrumb } from '@/components/ui/breadcrumb'
+
 export default function NewTransactionPage() {
   return (
-    <div></div>
+    <div className='max-w-screen-xl mx-auto py-10'>
+      <Breadcrumb></Breadcrumb>
+    </div>
   )
 }

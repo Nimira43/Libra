780dca51d8961d9638d74ea2c90f42190dbd6e4e
Nimira43 <n.rabone72@aol.com>
2025-05-23 03:07:26 +0100

    transactionsTable schema completed.

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, text } from 'drizzle-orm/pg-core'
+import { date, integer, numeric, pgTable, text } from 'drizzle-orm/pg-core'
 
 export const categoriesTable = pgTable('categories', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,4 +6,13 @@ export const categoriesTable = pgTable('categories', {
   type: text({
     enum: ['income', 'expense']
   }).notNull()
+})
+
+export const transactionsTable = pgTable('transactions', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: text('user_id').notNull(),
+  description: text().notNull(),
+  amount: numeric().notNull(),
+  transactionDate: date('transaction_date').notNull(),
+  categoryId: integer('category_id').references(() => categoriesTable.id).notNull(),  
 })

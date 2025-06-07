54ed045d4636aadb7778664d2c17cc6e0bca0a22
Nimira43 <n.rabone72@aol.com>
2025-06-07 02:04:13 +0100

    Getting categories from the database inside getCategories function.

---
+++ data/getCategories.ts
@@ -0,0 +1,7 @@
+import { db } from '@/db'
+import { categoriesTable } from '@/db/schema'
+import 'server-only'
+
+export async function getCategories() {
+  const categories = await db.select().from(categoriesTable)
+}

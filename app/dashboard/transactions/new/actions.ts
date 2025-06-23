14f472602813330a045070e4fe149a23caf455a5
Nimira43 <n.rabone72@aol.com>
2025-06-23 01:36:49 +0100

    Returning error if there is no user

--- app/dashboard/transactions/new/actions.ts
+++ app/dashboard/transactions/new/actions.ts
@@ -1,10 +1,20 @@
 'use server'
 
+import { auth } from '@clerk/nextjs/server'
+
 export const createTransaction = async (data: {
   amount: number
   transactionDate: string
   description: string
   categoryId: number
 }) => {
+  const {userId} = await auth()
+
+  if (!userId) {
+    return (
+      error: true,
+      message: 'Unauthorised'
+    )
+  }
 
 }

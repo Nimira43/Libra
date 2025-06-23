21f16c68c2ca97e1981cc4d6dfb3119f2c74fce7
Nimira43 <n.rabone72@aol.com>
2025-06-23 02:35:56 +0100

    Result

--- app/dashboard/transactions/new/new-transaction-form.tsx
+++ app/dashboard/transactions/new/new-transaction-form.tsx
@@ -3,6 +3,8 @@
 import TransctionForm, { transactionFormSchema } from '@/components/transaction-form'
 import { type Category } from '@/types/Category'
 import { z } from 'zod'
+import { createTransaction } from './actions'
+import { format } from 'date-fns'
 
 export default function NewTransactionForm ({
   categories
@@ -10,7 +12,12 @@ export default function NewTransactionForm ({
   categories: Category[]
 }) {
   const handleSubmit = async(data: z.infer<typeof transactionFormSchema>) => {
-
+    const result = await createTransaction({
+      amount: data.amount,
+      transactionDate: format(data.transactionDate, 'yyyy-MM-dd'),
+      categoryId: data.categoryId,
+      description: data.description
+    })
   }
   return (
     <TransctionForm 

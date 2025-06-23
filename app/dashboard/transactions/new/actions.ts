6696e7831cc77f1f096ad28f52ecd3fd08923821
Nimira43 <n.rabone72@aol.com>
2025-06-23 01:42:30 +0100

    transactionSchema - 1

--- app/dashboard/transactions/new/actions.ts
+++ app/dashboard/transactions/new/actions.ts
@@ -1,6 +1,17 @@
 'use server'
 
 import { auth } from '@clerk/nextjs/server'
+import { z } from 'zod'
+
+const transactionSchema = z.object({
+  amount: z
+    .number()
+    .positive('Amount must be greater than 0'),
+  description: z
+    .string()
+    .min(3, 'Description must contain at least 3 characters')
+    .max(300, 'Description must contain no more than 300 characters')
+})
 
 export const createTransaction = async (data: {
   amount: number
@@ -11,10 +22,9 @@ export const createTransaction = async (data: {
   const {userId} = await auth()
 
   if (!userId) {
-    return (
+    return {
       error: true,
       message: 'Unauthorised'
-    )
+    }
   }
-
 }

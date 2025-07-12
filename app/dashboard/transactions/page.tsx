a6528575fe7dbda59d89cf9e8b0726455c65aba0
Nimira43 <n.rabone72@aol.com>
2025-07-12 13:25:25 +0100

    Cpoied over breadcrumbs from NewTransactionPage

--- app/dashboard/transactions/page.tsx
+++ app/dashboard/transactions/page.tsx
@@ -1,5 +1,34 @@
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from "@/components/ui/breadcrumb";
+import { Link } from "lucide-react";
+
 export default function TransactionsPage() {
   return (
-    <div>Transactions Page</div>
+    <div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href='/dashboard'>
+                Dashboard
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href='/dashboard/transactions'>
+                Transactions
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>
+              New Transaction
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
   )
 }

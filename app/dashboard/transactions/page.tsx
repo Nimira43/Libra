c726da3bfa4e4fe0a4dc2281f44ddee21317e16c
Nimira43 <n.rabone72@aol.com>
2025-07-25 01:56:48 +0100

    month property

--- app/dashboard/transactions/page.tsx
+++ app/dashboard/transactions/page.tsx
@@ -10,7 +10,13 @@ const searchSchema = z.object({
   year: z.coerce
     .number()
     .min(today.getFullYear() - 100)
-    .max(today.getFullYear() + 1),
+    .max(today.getFullYear() + 1)
+    .catch(today.getFullYear),
+  month: z.coerce
+    .number()
+    .min(1)
+    .max(12)
+    .catch(today.getMonth() + 1)
   
 })
 

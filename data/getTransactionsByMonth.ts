baa5e7a16c4ec65f1b616b55f3ba80e544854e97
Nimira43 <n.rabone72@aol.com>
2025-08-07 23:45:20 +0100

    Created getTransactionsByMonth.ts - 7

--- data/getTransactionsByMonth.ts
+++ data/getTransactionsByMonth.ts
@@ -1,4 +1,7 @@
+import { db } from '@/db'
+import { transactionsTable } from '@/db/schema'
 import { auth } from '@clerk/nextjs/server'
+import { and, eq } from 'drizzle-orm'
 import 'server-only'
 
 export async function getTransactionsByMonth({
@@ -16,4 +19,8 @@ export async function getTransactionsByMonth({
 
   const earliestDate = new Date(year, month - 1, 1)
   const latestDate = new Date(year, month, 0)
+
+  const transactions = await db.select().from(transactionsTable).where(
+    and(eq(transactionsTable.userId, userId))
+  )
 }

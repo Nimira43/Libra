2dc7a0790b2e2be75b7291d9a3ee31d124df02e8
Nimira43 <n.rabone72@aol.com>
2025-08-07 23:37:45 +0100

    Created getTransactionsByMonth.ts - 3

--- data/getTransactionsByMonth.ts
+++ data/getTransactionsByMonth.ts
@@ -1,3 +1,11 @@
 import 'server-only'
 
-export async function getTransactionsByMonth() {}
+export async function getTransactionsByMonth({
+  month,
+  year
+}: {
+  month: number
+  year: number
+}) {
+
+}

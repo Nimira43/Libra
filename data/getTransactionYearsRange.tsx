e680a3d569f36964daf92e12180f974eb3476be8
Nimira43 <n.rabone72@aol.com>
2025-09-29 19:22:10 +0100

    getTransactionYearsRange - 4

--- data/getTransactionYearsRange.tsx
+++ data/getTransactionYearsRange.tsx
@@ -16,4 +16,17 @@ export async function getTransactionYearsRange() {
     .from(transactionsTable)
     .where(eq(transactionsTable.userId, userId))
     .orderBy(asc(transactionsTable.transactionDate))
+    .limit(1)
+
+  const today =  new Date()
+  const currentYear = today.getFullYear()
+  const earliestYear = earliestTransaction 
+    ? new Date(earliestTransaction.transactionDate).getFullYear()
+    : currentYear
+
+  const years = Array.from({ 
+    length: currentYear - earliestYear + 1
+  }).map((_, i) => currentYear - 1)
+
+  return years
 }

504e839cb3e3032953b90d2379d458ccef99f7aa
Nimira43 <n.rabone72@aol.com>
2025-09-29 18:56:40 +0100

    Month select - 5

--- app/dashboard/transactions/filters.tsx
+++ app/dashboard/transactions/filters.tsx
@@ -19,6 +19,7 @@ export default function Filters({
   return (
     <>
       <Select 
+        value={selectedMonth.toString()}
         onValueChange={(newValue) => setSelectedMonth(Number(newValue))}
       >
         <SelectTrigger>

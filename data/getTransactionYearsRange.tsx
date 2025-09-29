5a621adbab422eecc19db88f8b8d8b603c009b16
Nimira43 <n.rabone72@aol.com>
2025-09-29 19:09:03 +0100

    getTransactionYearsRange - 2

--- data/getTransactionYearsRange.tsx
+++ data/getTransactionYearsRange.tsx
@@ -1,5 +1,12 @@
+import { auth } from '@clerk/nextjs/server'
 import 'server-only'
 
 export async function getTransactionYearsRange() {
+  const { userId } = await auth()
+
+  if  (!userId) {
+    return []
+  }
+
   
 }

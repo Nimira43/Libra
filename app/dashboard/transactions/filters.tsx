7ab2360622fae4dfc1bda8cb39ad484edecd71b9
Nimira43 <n.rabone72@aol.com>
2025-09-29 18:42:33 +0100

    Passing in types.

--- app/dashboard/transactions/filters.tsx
+++ app/dashboard/transactions/filters.tsx
@@ -4,4 +4,12 @@ export default function Filters({
   year, 
   month,
   yearsRange
-})
+}: {
+  year: number
+  month: number
+  yearsRange: number[]
+}) {
+  return (
+    <div>Dropdowns: {year}, {month}</div>
+  )
+}

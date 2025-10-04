14d50eb96e55cdf2d1122c5b13a2846a1638c2a0
Nimira43 <n.rabone72@aol.com>
2025-10-04 15:54:57 +0100

    Copied over NewTransactionForm into EditTransactionForm and will update.

--- app/dashboard/transactions/[transactionId]/edit-transaction-form.tsx
+++ app/dashboard/transactions/[transactionId]/edit-transaction-form.tsx
@@ -1,1 +1,51 @@
+'use client'
 
+// import TransctionForm, { transactionFormSchema } from '@/components/transaction-form'
+// import { type Category } from '@/types/Category'
+// import { z } from 'zod'
+// import { createTransaction } from './actions'
+// import { format } from 'date-fns'
+// import { useToast } from '@/hooks/use-toast'
+// import { useRouter } from 'next/navigation'
+
+export default function EditTransactionForm ({
+  categories
+}: {
+  categories: Category[]
+}) {
+  const router = useRouter()
+  const {toast} = useToast()
+  const handleSubmit = async(data: z.infer<typeof transactionFormSchema>) => {
+    const result = await createTransaction({
+      amount: data.amount,
+      transactionDate: format(data.transactionDate, 'yyyy-MM-dd'),
+      categoryId: data.categoryId,
+      description: data.description
+    })
+
+    if (result.error) {
+      toast({
+        title: 'Error',
+        description: result.message,
+        variant: 'destructive'
+      }) 
+      return;    
+    }
+    toast({
+      title: 'Success',
+      description: 'Transaction Created.',
+      className: 'bg-green-500 text-light'   
+    }) 
+    router.push(`
+      /dashboard/transactions?month=${
+        data.transactionDate.getMonth() + 1
+      }&year=${data.transactionDate.getFullYear()}
+    `)
+  }
+  return (
+    <TransctionForm 
+      onSubmit={handleSubmit}  
+      categories={categories} 
+    />
+  )
+}

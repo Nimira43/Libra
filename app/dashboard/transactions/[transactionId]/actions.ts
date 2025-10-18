2793d0b4090e5cf505e7495147c5f0f21491f637
Nimira43 <n.rabone72@aol.com>
2025-10-18 11:51:00 +0100

    Props and types passed into updateTransaction

--- app/dashboard/transactions/[transactionId]/actions.ts
+++ app/dashboard/transactions/[transactionId]/actions.ts
@@ -1,3 +1,17 @@
 'use server'
 
-export async function updateTransaction() {}
+export async function updateTransaction({
+  id,
+  transactionDate,
+  description,
+  amount,
+  categooryId
+}: {
+  id: number
+  transactionDate: string
+  description: string
+  amount: number
+  categoryId: number
+}) {
+
+}

7e4122efc7d3003e80242f514d211165bb88f872
Nimira43 <n.rabone72@aol.com>
2025-11-08 22:06:22 +0000

    AlertDialog - 2

--- app/dashboard/transactions/[transactionId]/delete-transaction-dialog.tsx
+++ app/dashboard/transactions/[transactionId]/delete-transaction-dialog.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { AlertDialog, AlertDialogTrigger } from '@/components/ui/alert-dialog'
+import { AlertDialog, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
 import { PiTrash } from 'react-icons/pi'
 
@@ -14,10 +14,21 @@ export default function DeleteTransactionDialog({
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
-        <Button>
+        <Button 
+          variant='destructive'
+          size='icon'
+        >
           <PiTrash />
         </Button>
       </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure?
+          </AlertDialogTitle>
+
+        </AlertDialogHeader>
+      </AlertDialogContent>
     </AlertDialog>
   )
 }

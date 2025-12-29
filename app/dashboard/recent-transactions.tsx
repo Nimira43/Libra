a31908d78fb71869acd46a3497b20ba3e50565e7
Nimira43 <n.rabone72@aol.com>
2025-12-29 11:12:18 +0000

    RecentTransactions -  1

---
+++ app/dashboard/recent-transactions.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+
+export default function RecentTransactions() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <span>Recent Transactions</span>
+        </CardTitle>
+      </CardHeader>
+    </Card>
+  )
+}

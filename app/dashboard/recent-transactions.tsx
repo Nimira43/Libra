02a324d4faea6b59cfb0a5cec356042fcd4d5a76
Nimira43 <n.rabone72@aol.com>
2025-12-29 12:02:52 +0000

    Copied and updated table from TransactionPage and placed into RecentTransactions

--- app/dashboard/recent-transactions.tsx
+++ app/dashboard/recent-transactions.tsx
@@ -1,12 +1,15 @@
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table'
 import { getRecentTransactions } from '@/data/getRecentTransactions'
+import { Badge } from '@/components/ui/badge'
 import Link from 'next/link'
+import numeral from 'numeral'
+import { format } from 'date-fns'
 
 export default async function RecentTransactions() {
-  const recentTransactions = await getRecentTransactions()
-  console.log('RECENT TRANSACTIONS: ', recentTransactions)
-  
+  const transactions = await getRecentTransactions()
+
   return (
     <Card>
       <CardHeader>
@@ -26,7 +29,69 @@ export default async function RecentTransactions() {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        Recent Transactions
+        {!transactions?.length && (
+            <p className='py-10 text-center text-lg text-muted-foreground'>
+              There are no transaction for this month
+            </p>
+          )}
+          {!!transactions?.length && (
+            <Table className='mt-4'>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>
+                    Date
+                  </TableHead>
+                  <TableHead>
+                    Description
+                  </TableHead>
+                  <TableHead>
+                    Type
+                  </TableHead>
+                  <TableHead>
+                    Category
+                  </TableHead>
+                  <TableHead>
+                    Amount
+                  </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {transactions.map(transaction => (
+                  <TableRow
+                    key={transaction.id}  
+                  >
+                    <TableCell>
+                      {format(transaction.transactionDate,'dd MMMM yyyy')}
+                    </TableCell>
+                    <TableCell>
+                      {transaction.description}
+                    </TableCell>
+                    <TableCell
+                      className='uppercase'
+                    >
+                      <Badge
+                        className={
+                          transaction.transactionType === 'income'
+                            ? 'bg-green-600'
+                            : 'bg-red-600'
+
+                        }
+                      >
+                        {transaction.transactionType}
+                      </Badge>
+                      
+                    </TableCell>
+                    <TableCell>
+                      {transaction.category}
+                    </TableCell>
+                    <TableCell>
+                      £{numeral(transaction.amount).format('0,0[.]00')}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
       </CardContent>
     </Card>
   )

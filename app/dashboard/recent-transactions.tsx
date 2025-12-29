f954cb2f2067a918808a86164d122adc3b21066d
Nimira43 <n.rabone72@aol.com>
2025-12-29 11:21:14 +0000

    Corrected Link import

--- app/dashboard/recent-transactions.tsx
+++ app/dashboard/recent-transactions.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardTitle } from '@/components/ui/card'
-import { Link } from 'lucide-react'
+import Link from 'next/link'
 
 export default function RecentTransactions() {
   return (

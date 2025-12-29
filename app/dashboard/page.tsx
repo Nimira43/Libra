c281e32750907bf31f4a59a8fb3b7f806e00f7e7
Nimira43 <n.rabone72@aol.com>
2025-12-29 11:29:00 +0000

    Creating getRecentTransactions.ts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import RecentTransactions from './recent-transactions'
 export default function DashboardPage() {
   return (
     <div className='max-w-screen-xl mx-auto py-5'>
-      <h1 className='text-4xl font-medium pb-5'>Dashboard</h1>
+      <h1 className='text-3xl font-medium pb-5'>Dashboard</h1>
       <RecentTransactions />
     </div>
   )

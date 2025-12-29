691fe2913bd0a6d520c1436c9b07726a32ce145f
Nimira43 <n.rabone72@aol.com>
2025-12-29 11:20:21 +0000

    RecentTransactions - 2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import RecentTransactions from "./recent-transactions";
+
 export default function DashboardPage() {
   return (
     <div className='max-w-screen-xl mx-auto py-5'>
       <h1 className='text-4xl font-medium pb-5'>Dashboard</h1>
-      
+      <RecentTransactions />
     </div>
   )
 }
--- app/dashboard/recent-transactions.tsx
+++ app/dashboard/recent-transactions.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { Link } from 'lucide-react'
 
 export default function RecentTransactions() {
   return (
@@ -6,6 +8,17 @@ export default function RecentTransactions() {
       <CardHeader>
         <CardTitle>
           <span>Recent Transactions</span>
+          <div>
+            <Button
+              asChild
+              variant='outline'
+            >
+              <Link href='/dashboard/transactions'>View All</Link>
+            </Button>
+            <Button asChild>
+              <Link href='/dashboard/transactions/new'>Create New</Link>
+            </Button>
+          </div>
         </CardTitle>
       </CardHeader>
     </Card>

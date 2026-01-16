205af9446054e315315ccdde85ba12c25a5ae70d
Nimira43 <n.rabone72@aol.com>
2026-01-16 00:28:11 +0000

    Creating cashflow.tsx. Also created getAnnualCashflow.ts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import Cashflow from './cashflow'
 import RecentTransactions from './recent-transactions'
 
 export default function DashboardPage() {
   return (
     <div className='max-w-screen-xl mx-auto py-5'>
       <h1 className='text-3xl font-medium pb-5'>Dashboard</h1>
+      <Cashflow />
       <RecentTransactions />
     </div>
   )

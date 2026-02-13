7a977980c2a91dcf5f914a2b2534634cb1bc8161
Nimira43 <n.rabone72@aol.com>
2026-02-13 23:06:23 +0000

    Completed cashflow barchart

--- app/dashboard/cashflow-content.tsx
+++ app/dashboard/cashflow-content.tsx
@@ -1,7 +1,10 @@
 'use client'
 
-import { ChartContainer } from '@/components/ui/chart'
-import { Bar, BarChart } from 'recharts'
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
+import { format } from 'date-fns'
+import { Span } from 'next/dist/trace'
+import numeral from 'numeral'
+import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis } from 'recharts'
 
 export function CashflowContent({
   annualCashflow
@@ -11,7 +14,8 @@ export function CashflowContent({
     income: number
     expenses: number
   } []
-}) {
+  }) {
+  const today = new Date()
   return (
     <ChartContainer
       config={{
@@ -27,6 +31,54 @@ export function CashflowContent({
       className='w-full h-[300px]'
     >
       <BarChart data={annualCashflow}>
+        <CartesianGrid vertical={false} />
+        <YAxis tickFormatter={(value) => {
+          return `£${numeral(value).format('0,0')}`
+        }}/>
+        <XAxis tickFormatter={(value) => {
+          return (
+            format(
+              new Date(
+                today.getFullYear(),
+                value,
+                1
+              ),
+              'MMM'
+            )
+          )
+        }} />
+        <ChartTooltip
+          content={
+            <ChartTooltipContent
+              labelFormatter={(value, payload) => {
+                const month = payload[0]?.payload?.month
+                return (
+                  format(
+                    new Date(
+                      today.getFullYear(),
+                      month - 1,
+                      1
+                    ),
+                    'MMM'
+                  )
+                )
+              }}
+            />
+          }
+        />
+        <Legend
+          verticalAlign='top'
+          align='right'
+          height={30}
+          iconType='circle'
+          formatter={(value) => {
+            return (
+              <span  className='text-dark capitalize font-medium'>
+                {value}
+              </span>
+            )
+          }}
+        />
         <Bar
           dataKey='income'
           radius={3}
